4d5af06ae938318cbc9450c755ed4f62c9777085
GqmerBoy <143737643+GqmerBoy@users.noreply.github.com>
2024-11-13 17:31:46 -0500

    Test navmesh

---
+++ Assets/Sandbox/Nathan/Feature_navMesh/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _player = FindAnyObjectByType<ClickToMove>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.SetDestination(_player.position);
+    }
+}
